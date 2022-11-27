e6f7c5f57f4a3d28ee74c934c87e2e5f91f22e5f
UFFICIO\s.baig <saad.baig@hotmail.com>
2022-11-27 22:58:03 +0100

    commiting

---
+++ Source/MyShooter/MyShooterGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyShooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYSHOOTER_API AMyShooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
